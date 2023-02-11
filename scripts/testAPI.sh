c9eb9a62333bad1d69bd75c9d255de6c764b58ec
Joshua Swick <joshuaswickirl@gmail.com>
2023-02-11 15:24:20 -0700

    Add test command output

--- scripts/testAPI.sh
+++ scripts/testAPI.sh
@@ -7,3 +7,11 @@ curl -X POST localhost:8080 -d '{"record": {"value": "TGV0J3MgR28gIzMK"}}'
 curl -X GET localhost:8080 -d '{"offset": 0}'
 curl -X GET localhost:8080 -d '{"offset": 1}'
 curl -X GET localhost:8080 -d '{"offset": 2}'
+
+
+# {"offset":0}
+# {"offset":1}
+# {"offset":2}
+# {"record":{"value":"TGV0J3MgR28gIzEK","offset":0}}
+# {"record":{"value":"TGV0J3MgR28gIzIK","offset":1}}
+# {"record":{"value":"TGV0J3MgR28gIzMK","offset":2}}
