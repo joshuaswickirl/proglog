c2c361c709020d7a86d7ed79c74fa1e6a8c49f9b
Joshua Swick <joshuaswickirl@gmail.com>
2023-02-11 15:47:38 -0700

    Rename test and comment in script

--- scripts/testAPI.sh
+++ scripts/testAPI.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# This can be turned into acceptance tests
+
 curl -X POST localhost:8080 -d '{"record": {"value": "TGV0J3MgR28gIzEK"}}'
 curl -X POST localhost:8080 -d '{"record": {"value": "TGV0J3MgR28gIzIK"}}'
 curl -X POST localhost:8080 -d '{"record": {"value": "TGV0J3MgR28gIzMK"}}'
